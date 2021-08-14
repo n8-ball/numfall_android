33bebe8a137eb927741864aa4d098ff7659a0703
n8-ball <nateball17@gmail.com>
2021-08-14 15:31:13 -0700

    Matched colDelete to rowDelete

--- boosts/scripts/colDeleteBoost.gd
+++ boosts/scripts/colDeleteBoost.gd
@@ -1,18 +1,38 @@
 extends Node2D
 
+onready var colHighlight : Sprite = $"colHighlight"
+onready var sprite : Sprite = $"sprite"
+onready var animator : AnimationPlayer = $"animator"
 onready var confirm : AudioStreamPlayer = $"confirm"
 onready var deny : AudioStreamPlayer = $"deny"
-onready var sprite : Sprite = $"sprite"
+
 
 var board
 var button
 
-var scheduleDelete = false
+var activated = false
+var removeCoord = null
+var coordMod = 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	confirm.connect("finished", self, "_delete")
-	deny.connect("finished", self, "_delete")
+func deletePiece():
+	var chkPiece = null
+	while coordMod < board.brdHt:
+		chkPiece = board.board[coordMod][removeCoord.x]
+		if is_instance_valid(chkPiece):
+			chkPiece.setState(chkPiece.DELETE_STATE)
+			coordMod += 1
+			break;
+		coordMod += 1
+	if coordMod >= board.brdHt:
+		self.queue_free()
+
+func setReturnAnimation():
+	var returnAnimation = animator.get_animation("returnToButton")
+	returnAnimation.track_set_key_value(0, 0, self.position)
+	returnAnimation.track_set_key_value(0, 1, button.posNode.global_position)
+
+func alertButton():
+	button.displayUsage()
 
 func setBoard(newBoard):
 	board = newBoard
@@ -36,24 +56,37 @@ func _input(event):
 			var chkPiece = board.board[9][i]
 			if is_instance_valid(chkPiece) and i != coordSelect.x:
 				validMove = true
-				
+		
 		if coordSelect.x < board.brdWd && coordSelect.x >= 0 \
 		&& coordSelect.y < board.brdHt && coordSelect.y >= 0 \
 		&& is_instance_valid(board.board[coordSelect.y][coordSelect.x])\
-		&& not scheduleDelete && validMove:
-			confirm.play()
-			for i in range(10):
-				var chkPiece = board.board[i][coordSelect.x]
-				if is_instance_valid(chkPiece):
-					chkPiece.setState(chkPiece.DELETE_STATE)
-		elif not scheduleDelete:
-			deny.play()
-			button.displayUsage()
-		sprite.visible = false
-		scheduleDelete = true
+		&& not activated && validMove:
+			removeCoord = coordSelect
+			animator.play("deleteRow")
+			sprite.visible = false
+		
+		elif not activated:
+			setReturnAnimation()
+			animator.play("returnToButton")
+		activated = true
+		colHighlight.visible = false
 	
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and not activated:
 		self.position = event.position
+		var coordSelect = getCoord(event.position)
+		
+		var validMove = false
+		for i in range(6):
+			var chkPiece = board.board[9][i]
+			if is_instance_valid(chkPiece) and i != coordSelect.x:
+				validMove = true
 
-func _delete():
-	self.queue_free()
+		if coordSelect.x < board.brdWd && coordSelect.x >= 0 \
+		&& coordSelect.y < board.brdHt && coordSelect.y >= 0 \
+		&& is_instance_valid(board.board[coordSelect.y][coordSelect.x])\
+		&& not activated && validMove:
+			colHighlight.visible = true
+			colHighlight.global_position =\
+			Vector2(board.brdX + (coordSelect.x * board.pieceSize), board.brdY)
+		else:
+			colHighlight.visible = false
